fd7f07e84f848b958d4afb3629e425349605505b
choi <ppaccomy@gmail.com>
2024-02-12 22:08:06 +0900

    Section2 - 19.Skill AI

--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Protobuf.Protocol;
+using Server.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -66,6 +67,7 @@ namespace Server.Game
             State = CreatureState.Moving;
         }
 
+        int _skillRange = 1;
         long _nextMoveTime = 0;
         protected virtual void UpdateMoving()
         {
@@ -78,14 +80,17 @@ namespace Server.Game
             {
                 _target = null;
                 State = CreatureState.Idle;
+                BroadcastMove();
                 return;
             }
 
-            int dist = (_target.CellPos - CellPos).cellDistFromZero;
+            Vector2Int dir = _target.CellPos - CellPos;
+            int dist = dir.cellDistFromZero;
             if(dist == 0 || dist > _chaseCellDist)
             {
                 _target = null;
                 State = CreatureState.Idle;
+                BroadcastMove();
                 return;
             }
 
@@ -94,6 +99,16 @@ namespace Server.Game
             {
                 _target = null;
                 State = CreatureState.Idle;
+                BroadcastMove();
+                return;
+            }
+
+            //스킬로 넘어갈지 체크
+            if(dist <= _skillRange && (dir.x == 0 || dir.y == 0))
+            {
+                State = CreatureState.Skill;
+                _coolTime = 0;
+                //BroadcastMove();
                 return;
             }
 
@@ -102,15 +117,70 @@ namespace Server.Game
             Room.Map.ApplyMove(this, path[1]); 
             
             //다른 플레이어에게 알림
+            BroadcastMove();
+        }
+
+        void BroadcastMove()
+        {
             S_Move movePacket = new S_Move();
             movePacket.ObjectId = Id;
             movePacket.PosInfo = PosInfo;
             Room.Broadcast(movePacket);
         }
 
+        long _coolTime = 0;
         protected virtual void UpdateSkill()
         {
-            //TODO : 스킬 시전
+            if(_coolTime == 0)
+            {
+                //유효한 타겟인지 체크
+                if(_target == null || _target.Room != Room || _target.Hp == 0)
+                {
+                    _target = null;
+                    State = CreatureState.Moving;
+                    BroadcastMove();
+                    return;
+                }
+
+                //스킬이 아직 사용 가능한지
+                Vector2Int dir = _target.CellPos - CellPos;
+                int dist = dir.cellDistFromZero;
+                bool canUseSkill = dist <= _skillRange && (dir.x == 0 || dir.y == 0);
+                if(canUseSkill == false)
+                {
+                    State = CreatureState.Moving;
+                    BroadcastMove();
+                    return;
+                }
+
+                //타겟팅 방향 주시
+                MoveDir lookDir = GetDirFromVec(dir);
+                if(Dir != lookDir)
+                {
+                    Dir = lookDir;
+                    BroadcastMove();
+                }
+                Skill skillData = null;
+                DataManager.SkillDict.TryGetValue(1, out skillData); //1번 스킬 데이터를 가져온다.
+
+                //데미지 판정
+                _target.OnDamaged(this, skillData.damage + Stat.Str);
+
+                //스킬사용 broadcast
+                S_Skill skill = new S_Skill() { Info = new SkillInfo() };
+                skill.ObjectId = Id;
+                skill.Info.SkillId = skillData.id;
+                Room.Broadcast(skill);
+
+                //스킬 쿨타임 적용
+                int coolTime = (int)(1000*skillData.cooldown);
+                _coolTime = Environment.TickCount64 + coolTime;
+            }
+
+            if(_coolTime > Environment.TickCount64)
+                return;
+
+            _coolTime = 0;
         }
 
         protected virtual void UpdateDead()
