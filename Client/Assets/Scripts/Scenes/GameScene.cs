6e2d2e546df49fbdd74a8b6e310d1dcef35b16db
choi <ppaccomy@gmail.com>
2024-02-21 22:16:33 +0900

    Part9: Section1 - 9.Inventory #1

--- Client/Assets/Scripts/Scenes/GameScene.cs
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    UI_GameScene _gameSceneUI;
     protected override void Init()
     {
         base.Init();
@@ -12,22 +13,11 @@ public class GameScene : BaseScene
 
         Managers.Map.LoadMap(1);
 
-        //GameObject player = Managers.Resource.Instantiate("Creature/Player");
-        //player.name = "Player";
-        //Managers.Object.Add(player);
+        Screen.SetResolution(640, 480, false);
+
+        _gameSceneUI = Managers.UI.ShowSceneUI<UI_GameScene>();
+
         
-
-        //Managers.UI.ShowSceneUI<UI_Inven>();
-        //Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
-        //gameObject.GetOrAddComponent<CursorController>();
-
-        //GameObject player = Managers.Game.Spawn(Define.WorldObject.Player, "UnityChan");
-        //Camera.main.gameObject.GetOrAddComponent<CameraController>().SetPlayer(player);
-
-        ////Managers.Game.Spawn(Define.WorldObject.Monster, "Knight");
-        //GameObject go = new GameObject { name = "SpawningPool" };
-        //SpawningPool pool = go.GetOrAddComponent<SpawningPool>();
-        //pool.SetKeepMonsterCount(2);
     }
 
     public override void Clear()
